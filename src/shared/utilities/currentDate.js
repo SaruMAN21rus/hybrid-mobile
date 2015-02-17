b3b6ff2d14ccc472dafb952951b4fff2c6f03706
Alexey Mukin <amukin@cheby.ru>
2015-02-17 17:20:05 +0300

    1

---
+++ src/shared/utilities/currentDate.js
@@ -0,0 +1,10 @@
+(function(app){
+
+	'use strict';
+	
+	var date = new Date();
+
+	app.currentDate = ('0' + date.getDate()).slice(-2) + '.' + ('0' + (date.getMonth() + 1)).slice(-2) + '.' + date.getFullYear();
+	$('#date').text(app.currentDate);
+
+})(app); //pass in global namespace
