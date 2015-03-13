d778c4b7b0b502053f1d9c9b8e77f3f4ae2d8acf
Alexey Mukin <amukin@cheby.ru>
2015-03-13 17:20:54 +0300

    11

--- src/ui/menu/rightMenu/rightMenuViewModel.js
+++ src/ui/menu/rightMenu/rightMenuViewModel.js
@@ -5,29 +5,58 @@
     //below we create the viewModel 
     app.rightMenu = { //create viewModel namespace in global i.e. namespace.[viewModel Name], to expose to global
         viewModel: kendo.observable({
+            init: function(e) {
+                var buttons = $("#right-menu .km-button");
+                var menu = $("#right-menu");
+                
+                var buttonSize = $(window).height()/9;
+                app.rightMenu.menuWidth = buttonSize;
+                menu.width(buttonSize);
+                console.log(buttonSize);
+                buttons.each(
+                    function () {
+                        $(this).height(buttonSize-2);
+                        $(this).width(buttonSize-2)
+                        $(this).css({
+                            fontSize: buttonSize*0.7,
+                            lineHeight: buttonSize+"px"
+                        });
+                    }
+                );
+
+                $(window).resize(function() {
+                    var buttonSize = window.height/9;
+                    app.rightMenu.menuWidth = buttonSize;
+                    menu.width(buttonSize);
+                    buttons.each(
+                        function () {
+                            $(this).height(buttonSize-2);
+                            $(this).width(buttonSize-2)
+                        }
+                    );
+                });
+            },
             showRightMenu: function(e){
-                var drawer = $("#right-menu").data("kendoMobileDrawer")
-                this.drawer = drawer;
-                if (!drawer.visible) {
-                    drawer.show();
+                var drawer = $("#right-menu");
+                var view = app.app.view();
+                var visible = app.rightMenu.isVisible;
+                var menuWidth = app.rightMenu.menuWidth;
+                if (!visible) {
+                    app.rightMenu.isVisible = true;
+                    drawer.show({duration: 400});
+                    $(view.id).animate({
+                        width: "-=" + menuWidth
+                    }, 400);
                 } else {
-                    drawer.hide();
+                    app.rightMenu.isVisible = false;
+                    $(view.id).animate({
+                        width: "+=" + menuWidth
+                    }, 400);
+                    drawer.hide({duration: 400});
                 }
             },
-            onShow: function(e) {
-                $(app.app.view().id).animate({
-                    width: "-=100",
-                    left: "+=100"
-                }, 0);
-            },
-            onHide: function(e) {
-                if (typeof app.app != "undefined") {
-                    $(app.app.view().id).animate({
-                        width: "+=100",
-                        left: "-=100"
-                    }, 0);
-                }
-            },
+            isVisible: false,
+            menuWidth: 50
         })
     };
-})(app); //pass in global namespace
+})(app);
